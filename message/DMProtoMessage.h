79ab88a7f36358384fdf5aea0fb5d30056c60447
bing <563853086@qq.com>
2016-09-20 13:56:08 -0700

    add message

---
+++ message/DMProtoMessage.h
@@ -0,0 +1,19 @@
+//=============================================================================
+/* 
+*  File: DMProtoMessage.h
+*
+*  Author: bing
+*
+*  Date: 2016-09-21
+*
+*  Version: v2.0
+*
+*  Github/Mail: https://github.com/binchen-china    <563853086@qq.com>
+*
+*  Note:
+*/
+//=============================================================================
+
+#pragma once
+
+#include "GateService.pb.h"
